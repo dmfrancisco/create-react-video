e56eea43f8e6b9ca0a911cd6a0e19317a2bd31ac
David Francisco <dmfrancisc@gmail.com>
2017-08-07 23:13:31 +0100

    Minor updates to global styles

--- packages/gideo/src/global.js
+++ packages/gideo/src/global.js
@@ -1,8 +1,15 @@
 import { injectGlobal } from 'styled-components';
 
 injectGlobal`
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
   body {
     margin: 0;
+    -webkit-font-smoothing: antialiased;
   }
   div {
     position: absolute;
