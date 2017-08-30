b958b66f7dad5c0394702485c4de5b2062a1dc1c
David Francisco <dmfrancisc@gmail.com>
2017-08-30 18:01:52 +0100

    Update react-scripts template

--- packages/react-scripts/template/src/index.js
+++ packages/react-scripts/template/src/index.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Gideo from '@robo54/gideo';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Gideo, { Audio, Video, animatable } from "@robo54/gideo";
+
+const Text = props => {
+  const style = {
+    alignItems: "center",
+    display: "flex",
+    height: "100%",
+    justifyContent: "center"
+  };
+  return (
+    <div style={style}>
+      {props.children}
+    </div>
+  );
+};
 
 class Project extends Component {
   render() {
     return (
-      <Gideo aspectRatio={1} duration={10}>
-        Hello, World!
+      <Gideo width={600} height={600} duration={10}>
+        <Text>Hello, World!</Text>
       </Gideo>
     );
   }
 }
 
-ReactDOM.render(<Project />, document.getElementById('root'));
+ReactDOM.render(<Project />, document.getElementById("root"));
